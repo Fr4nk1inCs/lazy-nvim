79fca5c3e2930c29f7319d870daa9fc6186f1b32
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-08-02 20:58:41 +0800

    remove tree-sitter `ensure_installed`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,26 +6,6 @@ return {
             "HiPhish/nvim-ts-rainbow2",
         },
         opts = {
-            ensure_installed = {
-                "bash",
-                "c",
-                "vimdoc",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "yaml",
-                "latex",
-            },
             rainbow = {
                 enable = true,
                 -- list of languages you want to disable the plugin for
