c7739b4978790aaefe03f60215d87fa80e8414da
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-10-18 20:25:26 +0800

    Bump lazyvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,8 +1,9 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
-  -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+    -- bootstrap lazy.nvim
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -20,6 +21,8 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.rust" },
         { import = "lazyvim.plugins.extras.lang.yaml" },
 
+        { import = "lazyvim.plugins.extras.lsp.none-ls" },
+
         { import = "lazyvim.plugins.extras.dap.core" },
 
         -- { import = "lazyvim.plugins.extras.formatting.prettier" },
@@ -27,6 +30,7 @@ require("lazy").setup({
 
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
         { import = "lazyvim.plugins.extras.ui.edgy" },
+        { import = "lazyvim.plugins.extras.ui.alpha" },
 
         { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -12,7 +12,6 @@ return {
                     nls.builtins.diagnostics.fish,
                     nls.builtins.formatting.stylua,
                     nls.builtins.formatting.shfmt,
-                    nls.builtins.formatting.autopep8,
                     nls.builtins.formatting.clang_format,
                     nls.builtins.formatting.rustfmt.with({ extra_args = { "--edition=2021" } }),
                     nls.builtins.formatting.eslint,
@@ -27,18 +26,21 @@ return {
             "williamboman/mason.nvim",
             "nvimtools/none-ls.nvim",
         },
-        config = function()
-            require("mason-null-ls").setup({
-                automatic_installation = true,
-                automatic_setup = true,
-                handlers = nil,
-            })
-        end,
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "fish_indent",
+                "shfmt",
+                "eslint",
+            },
+            automatic_installation = true,
+            automatic_setup = true,
+            handlers = nil,
+        },
     },
     {
         "neovim/nvim-lspconfig",
         opts = {
-            autoformat = true,
             format = { timeout_ms = 3000 },
         },
     },
