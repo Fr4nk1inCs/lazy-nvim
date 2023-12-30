1534b594f963f2b53650bcadaa53991f5783d567
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-12-31 03:13:21 +0800

    Enable highlight of winbar

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -7,6 +7,7 @@ return {
         "SmiteshP/nvim-navic",
         dependencies = "neovim/nvim-lspconfig",
         opts = {
+            highlight = true,
             lsp = {
                 auto_attach = true,
             },
