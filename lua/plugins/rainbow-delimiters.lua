bf985202fb053f1f4a24b9564e0208fdb4033e4d
Fr4nk1in <fushen@mail.ustc.edu.cn>
2023-12-31 03:13:02 +0800

    Enable rainbow delimiters

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -5,28 +5,23 @@ end
 return {
     {
         "HiPhish/rainbow-delimiters.nvim",
-        enabled = false,
         config = function()
-            -- This module contains a number of default definitions
-            local rainbow_delimiters = require("rainbow-delimiters")
-
             vim.g.rainbow_delimiters = {
-                strategy = {
-                    [""] = rainbow_delimiters.strategy["global"],
-                    commonlisp = rainbow_delimiters.strategy["local"],
-                },
                 query = {
                     [""] = "rainbow-delimiters",
                     lua = "rainbow-blocks",
+                    latex = "rainbow-blocks",
+                    verilog = "rainbow-blocks",
+                    query = "rainbow-blocks",
                 },
                 highlight = {
-                    "RainbowDelimiterRed",
-                    "RainbowDelimiterYellow",
-                    "RainbowDelimiterBlue",
-                    "RainbowDelimiterOrange",
-                    "RainbowDelimiterGreen",
-                    "RainbowDelimiterViolet",
-                    "RainbowDelimiterCyan",
+                    "TSRainbowRed",
+                    "TSRainbowYellow",
+                    "TSRainbowBlue",
+                    "TSRainbowOrange",
+                    "TSRainbowGreen",
+                    "TSRainbowViolet",
+                    "TSRainbowCyan",
                 },
             }
         end,
