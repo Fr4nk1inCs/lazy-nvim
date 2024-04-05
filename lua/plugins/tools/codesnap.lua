6b93ff38879ae3623f17d359010b2b9f83775278
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-04-06 03:07:30 +0800

    codesnap.nvim

---
+++ lua/plugins/tools/codesnap.lua
@@ -0,0 +1,21 @@
+local save_path = "~/Pictures/Codesnap"
+if vim.fn.has("wsl") then
+    save_path = "/mnt/c/Users/fushen/Pictures/CodeSnap"
+end
+
+if vim.fn.isdirectory(save_path) == 0 then
+    vim.fn.mkdir(save_path, "p")
+end
+
+return {
+    {
+        "mistricky/codesnap.nvim",
+        build = "make",
+        opts = {
+            save_path = save_path,
+            has_breadcrumbs = true,
+            bg_theme = "sea",
+            code_font_family = "Maple Mono SC NF",
+        },
+    },
+}
