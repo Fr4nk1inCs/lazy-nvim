0ba04e0dad68bf64ab5c30bda8e47e090778f9d8
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-06-09 06:07:43 +0800

    Fix session manager

--- lua/plugins/ui/startup.lua
+++ lua/plugins/ui/startup.lua
@@ -344,9 +344,12 @@ return {
             {
                 "Shatur/neovim-session-manager",
                 event = "VeryLazy",
-                opts = {
-                    autoload_mode = "Disabled",
-                },
+                config = function()
+                    local config = require("session_manager.config")
+                    require("session_manager").setup({
+                        autoload_mode = config.AutoloadMode.Disabled,
+                    })
+                end,
             },
         },
     },
