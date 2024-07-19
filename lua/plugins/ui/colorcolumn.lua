fc827dd7e04d9e5761c8ff180c712307c354a886
Fr4nk1in <fushen@mail.ustc.edu.cn>
2024-07-20 07:06:23 +0800

    better color column

---
+++ lua/plugins/ui/colorcolumn.lua
@@ -0,0 +1,24 @@
+return {
+    {
+        "m4xshen/smartcolumn.nvim",
+        opts = {
+            disabled_filetypes = {
+                "help",
+                "text",
+                "tex",
+                "markdown",
+                "dashboard",
+                "lazy",
+                "mason",
+                "neo-tree",
+                "help",
+                "checkhealth",
+                "lspinfo",
+                "noice",
+                "Trouble",
+                "fish",
+                "zsh",
+            },
+        },
+    },
+}
